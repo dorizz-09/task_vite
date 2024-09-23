38bbbcbc7db6d05f18e19f37e8610c758f435502
dorizz-09 <ddwipaskah@gmail.com>
2024-09-23 09:04:02 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import $ from 'jquery'
+
+$(document).ready(function () {
+  $("#okButton").click(function () {
+    $(".message").text("Haii kenalin aku Dorizzz dari angkatan 2022");
+  });
+});
